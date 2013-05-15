f1df90513ecac52d69b934872c76de2fd98a2855
Hillel Arnold <helrond@hotmail.com>
2013-05-15 19:59:03 -0400

    Create modal.js

---
+++ script/modal.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+$('a.song-list-title').bind('click', function() {
+   var $this = $(this);
+   var outputHolder = $("<div class='home'></div>");
+   $("body").append(outputHolder);
+   outputHolder.load($this.attr("href"), null, function() {
+      outputHolder.dialog(// whatever params you want);
+   });
+   return false;
+});
+});
