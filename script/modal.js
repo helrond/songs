886e92f8321de31f78ee5e9ccba42d11c49fe490
Hillel Arnold <helrond@hotmail.com>
2013-05-15 21:03:12 -0300

    Update modal.js

--- script/modal.js
+++ script/modal.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-$('a.song-list-title').bind('click', function() {
+$('.song-list-title a').bind('click', function() {
    var $this = $(this);
    var outputHolder = $("<div class='home'></div>");
    $("body").append(outputHolder);
