c11a2f3bdda494324651bda6b7ba4d8f4b910e90
helrond <helrond@hotmail.com>
2013-05-24 17:41:35 -0400

    get the comma in the right place

--- script/showhide.js
+++ script/showhide.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $('.song-list-title').click(function(){
-$('.content').css("display," "none");
+$('.content').css("display", "none");
 $(this).parent().next().children().slideToggle("slow")
 });
 });
