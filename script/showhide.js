deaf8070750b210e0ebc755d7848275d62ac1cab
helrond <helrond@hotmail.com>
2013-05-24 17:57:39 -0400

    trying different jquery effect and css tweaking

--- script/showhide.js
+++ script/showhide.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 $('.song-list-title').click(function(){
 $('.content').css("display", "none");
-$(this).parent().next().children().slideToggle("slow")
+var $lefty = $(this).parent().next().children();
+$lefty.animate({
+      left: parseInt($lefty.css('left'),10) == 0 ?
+        -$lefty.outerWidth() :
+        0
+    });
 });
 });
