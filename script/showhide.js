4be003ba69d08d2888b2593ff3aac1cb3c421cb6
helrond <helrond@hotmail.com>
2013-05-24 18:52:10 -0400

    close"

--- script/showhide.js
+++ script/showhide.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $('.song-list-title').click(function(){
-$('.song-list-content').css("display", "none");
+$('.song-list-content').css({"display: none", "background-color: #ffffff"});
 $(this).next().slideToggle();
 $(this).css('background-color', '#999999');
 });
