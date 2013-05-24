99d6090df9b1c9c512d46624b1318cf28b45469b
helrond <helrond@hotmail.com>
2013-05-24 18:42:03 -0400

    making minor changes to appearane

--- script/showhide.js
+++ script/showhide.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $('.song-list-title').click(function(){
 $('.song-list-content').css("display", "none");
-$(this).next().slideToggle();
+$(this).next().slideToggle().css('background-color', 'blue');
 });
 });
