29a7332f847b0fb25ccc72eec193af0a76c3a68a
helrond <helrond@hotmail.com>
2013-05-24 18:01:43 -0400

    making it simpler

--- script/showhide.js
+++ script/showhide.js
@@ -1,11 +1,6 @@
 $(document).ready(function(){
 $('.song-list-title').click(function(){
 $('.content').css("display", "none");
-var $lefty = $(this).parent().next().children();
-$lefty.animate({
-      left: parseInt($lefty.css('left'),10) == 0 ?
-        -$lefty.outerWidth() :
-        0
-    });
+$(this).parent().next().children().animate({width: 'toggle'});
 });
 });
