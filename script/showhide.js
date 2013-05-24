33a445af04004f6bd59152beef4facdbe16debff
helrond <helrond@hotmail.com>
2013-05-24 18:12:30 -0400

    going back to original animation

--- script/showhide.js
+++ script/showhide.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $('.song-list-title').click(function(){
 $('.content').css("display", "none");
-$(this).next().animate({width: 'toggle'});
+$(this).next().slideToggle();
 });
 });
