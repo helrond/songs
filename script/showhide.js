b4830216496ddf1730997ca51e5532835cfe915a
helrond <helrond@hotmail.com>
2013-05-25 21:49:00 -0400

    getting script to work right

--- script/showhide.js
+++ script/showhide.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
     if ($(window).width() > 480) {
         $('.song-list-title a').prop('href', '#');
-        $('.song-list-title').hover(function () {
-            $('.song-list-title').css('background-color', '#eeeeee');
+        $('.song-list-title').mouseenter(function () {
+            $(this).css('background-color', '#eeeeee');
+        });
+        $('.song-list-title').mouseleave(function () {
+            $(this).css('background-color', '#ffffff');
         });
         $('.song-list-title').click(function () {
             $('.song-list-content').css('display', 'none');
