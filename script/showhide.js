c6621b94296bfc7818a17b19f6fea3e62bfec154
helrond <helrond@hotmail.com>
2013-05-25 22:51:47 -0400

    one last try

--- script/showhide.js
+++ script/showhide.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     if ($(window).width() > 480) {
         $('.song-list-title a').prop('href', '#');
         $('.song-list-title').mouseenter(function () {
-            $(this).css('background-color', '#ddd');
+            $(this).addClass("hover");
         });
         $('.song-list-title').mouseleave(function () {
-            $(this).css('background-color', 'none');
+            $(this).removeClass("hover");
         });
         $('.song-list-title').click(function (event) {
         event.preventDefault();
