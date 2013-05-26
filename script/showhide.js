8de89692b2940b72eb83ac1333a20bc23bd9e379
Hillel Arnold <helrond@hotmail.com>
2013-05-25 21:32:54 -0300

    Update showhide.js

--- script/showhide.js
+++ script/showhide.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
     if ($(window).width() > 480) {
-$(".song-list-title a").attr("href", "#");
-$('.song-list-title').click(function(){
-$('.song-list-content').css({"display: none", "background-color: #ffffff"});
-$(this).next().slideToggle();
-$(this).css('background-color', '#999999');
-}
-});
+        $(".song-list-title a").attr("href", "#");
+        $('.song-list-title').click(function () {
+            $('.song-list-content').css({
+                "display: none": "background-color: #ffffff"
+            });
+            $(this).next().slideToggle();
+            $(this).css('background-color', '#999999');
+        });
+    };
 });
