21951c9d068bcc5f0f20fdf44520694ac5922484
helrond <helrond@hotmail.com>
2013-05-25 22:46:44 -0400

    css tweaking

--- script/showhide.js
+++ script/showhide.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     if ($(window).width() > 480) {
         $('.song-list-title a').prop('href', '#');
         $('.song-list-title').mouseenter(function () {
-            $(this).css('background-color', '#eeeeee');
+            $(this).css('background-color', '#ddd');
         });
         $('.song-list-title').mouseleave(function () {
-            $(this).css('background-color', '#ffffff');
+            $(this).css('background-color', 'none');
         });
         $('.song-list-title').click(function (event) {
         event.preventDefault();
