08b20cd6a5bbe5a82b0878b0f146e7af767ce08c
Hillel Arnold <helrond@hotmail.com>
2013-05-25 21:10:03 -0400

    Update showhide.js

--- script/showhide.js
+++ script/showhide.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     if ($(window).width() > 480) {
         $("a.song-list-title").prop("href", "#");
         $('.song-list-title').click(function () {
-            $('.song-list-content').css('display', 'none;');
+            $('.song-list-content').css('display', 'none');
             $(this).css('background-color', '#ffffff');
             $(this).next().slideToggle();
             $(this).css('background-color', '#999999');
