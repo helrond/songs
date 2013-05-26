b02b63ddc78ecad4a2a499f5e88467aef91573de
helrond <helrond@hotmail.com>
2013-05-25 21:43:50 -0400

    getting hex codes right maybe this time

--- script/showhide.js
+++ script/showhide.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     if ($(window).width() > 480) {
         $('.song-list-title a').prop('href', '#');
         $('.song-list-title').hover(function () {
-            $('.song-list-title').css('background-color', '#e9e9e9');
+            $('.song-list-title').css('background-color', '#eeeeee');
         });
         $('.song-list-title').click(function () {
             $('.song-list-content').css('display', 'none');
             $('.song-list-title').css('background-color', '#ffffff');
             $(this).next().slideToggle();
-            $(this).css('background-color', '#d9d9d9');
+            $(this).css('background-color', '#dddddd');
         });
     }
 });
