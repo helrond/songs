68ec69e056c7e51ac41fca48411ed3097995d512
helrond <helrond@hotmail.com>
2015-04-05 12:52:39 -0400

    bootstrapping, updating jekyll, improving print, etc

--- script/dropdown.js
+++
@@ -1,7 +0,0 @@
-window.onload=function() {
-  document.getElementById("menutitle").onclick=function(){
-      var showDiv = document.getElementById('showDiv');
-      showDiv.style.display = (showDiv.style.display  == 'none')?'block':'none';
-  }
-  document.getElementById('showDiv').style.display="none"; // not accessible by script until we set it
-}
--- script/showhide.js
+++ script/showhide.js
@@ -1,17 +1,33 @@
 $(document).ready(function () {
-    if ($(window).width() > 480) {
-        $('.song-list-title').mouseenter(function () {
-            $(this).addClass("hover");
-        });
-        $('.song-list-title').mouseleave(function () {
-            $(this).removeClass("hover");
-        });
+    if ($(window).width() > 768) {
         $('.song-list-title').click(function (event) {
-        event.preventDefault();
-            $('.song-list-content').css('display', 'none');
-            $('.song-list-title').removeClass('select');
-            $(this).next().slideToggle();
-            $(this).addClass('select');
+            event.preventDefault();
+            $('.song-list-content').hide();
+            var id = $(this).attr('data-identifier');
+            var scrolltop = $(window).scrollTop();
+            var height = $(window).height()-10;
+            $('.song-list-content').each(function(){
+                if ($(this).attr('data-identifier') === id) {
+                    if (scrolltop === 0) {
+                        $(this).css('top', scrolltop+57)
+                        $(this).css('min-height', height-52);
+                    } else if (scrolltop <= 57) {
+                        $(this).css('top', scrolltop + (57-scrolltop));
+                        $(this).css('min-height', height-(52-scrolltop));
+                    } else {
+                        $(this).css('top', scrolltop+5)
+                        $(this).css('min-height', height);
+                    }
+                    
+                    $(this).show();
+                }
+            });
+        });
+        $('.song-print').on('click', function(){
+            window.print();
+        });
+        $('.list-print').on('click', function(){
+            window.print();
         });
     }
 });
